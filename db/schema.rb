64d45b3377c39ad5ba84ef80d2a0b3046a9724c2
adamzermane <adamzermane92@yahoo.com>
2021-12-07 15:34:12 +0100

    shopping_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_111448) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.boolean "completed"
-    t.string "details"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_211_207_111_448) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.boolean 'completed'
+    t.string 'details'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
